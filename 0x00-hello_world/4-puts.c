631bbf370d412153ca4a87ddbcf0bebb3143a35f
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-08 10:22:33 -0700

    Hello Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
+/** *main-Entry *Return:Always0(success)*/
 int main(void)
 {
-	 puts("  \" Programming is like building a multilingual puzzle \n");
-
-	   return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
