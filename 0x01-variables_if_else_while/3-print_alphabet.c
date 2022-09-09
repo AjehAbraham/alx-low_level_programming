bcd721d65fcab35de0187be9f33617e71e7d9560
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-09 14:08:53 -0700

    letters reverse

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - printf value n
+* Return: 0
+*/
+int main(void)
+{
+char b;;
+char Z;
+for(b='a'; b<='z'; ch++)
+for (Z=‘A’; Z<=‘Z’; Z++)
+putchar("b,Z");
+putchar(“\n”);
+return 0;
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+nclude<stdio.h>
+/**
+* main - printf value n
+* Return: 0
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i >= 'z'; i--)
+putchar("%c", i);
+putchar("\n");
+return (0);
+}
