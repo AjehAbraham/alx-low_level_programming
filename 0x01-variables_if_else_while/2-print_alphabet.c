d2a13e11e73f9e0b2ef9314cd42ff204a7cf5f6c
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2022-09-09 22:46:04 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+/**
+* main - printf value n
+* Return: 0
+*/
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar("%c", ch);
+putchar("\n");
+return (0);
+}
