9751d00751d8bb5818a94b8042d16e83ddcedf71
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-13 09:01:29 -0700

    printing a-z using loop in C

--- 0x02-functions_nested_loops/2-print_alphabet_10x.c
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-* print_alphabet_10x - entry
+* print_alphabet_x10 - entry
 * Return: always 0 (success)
 */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 char i, j;
-for (k = 0; k <= 10 ; k++)
+for (j = 0; j < 10 ; j++)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
-{
+}
 _putchar('\n');
 }
 }
