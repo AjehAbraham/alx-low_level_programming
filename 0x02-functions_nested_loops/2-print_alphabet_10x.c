8429ce4359804a5c57f38bc58a1682b4c1a4603b
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-13 08:41:38 -0700

    using a function called print alphabet in C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet_10x - entry
+* Return: always 0 (success)
+*/
+void print_alphabet_10x(void)
+{
+char i, j;
+for (k = 0; k <= 10 ; k++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+{
+_putchar('\n');
+}
+}
