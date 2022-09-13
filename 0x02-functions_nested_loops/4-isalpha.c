9fc325ec1cfc00e7bea95e4b3c5ae81508244df0
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-13 10:02:25 -0700

    checkiung for aphabet usinfg conditional statement

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isalpha - checking for alphabet
+*  @c:using the function c to check
+* Return:0 or 1
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
