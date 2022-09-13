c6d0884a5bbfac014decba579dec3bf5e27ef687
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-13 09:36:37 -0700

    checking if lower case is present in C

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - check for lower case 
+* @c:check for lowercase statement
+* Return:0
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
