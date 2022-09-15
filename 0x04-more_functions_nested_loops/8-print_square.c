6702e83d013373c5774a4d3875a8c93f07cdf79d
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2022-09-15 23:39:49 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_square - check code
- * @size:print sqaure if condition is true
- * Return:0
- */
+* print_square - check code
+* @size:print sqaure if condition is true
+* Return:0
+*/
 void print_square(int size)
 {
-	int i;
+int i;
 {
 for (i = 1; i <= size; i++)
 _putchar('#');
