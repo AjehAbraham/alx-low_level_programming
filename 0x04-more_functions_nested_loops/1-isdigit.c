27e778506fb8f51a6c9ee17551df94113e64379a
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-15 06:43:14 -0700

    printing numbers from 0-9 else  return 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isdigit - check code 
+* @c:use to check for digit
+* Return:1 if true else return 0
+*/
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
