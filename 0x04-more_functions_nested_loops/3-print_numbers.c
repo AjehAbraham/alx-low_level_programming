46a2b16e4fcde613880b0c74fec59b666502905a
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2022-09-15 15:16:03 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,10 @@
 */
 void print_numbers(void)
 {
-	int i;
-	for (i >= 48; i <= 57; i++)
-
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+int i;
+for (i >= 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
