a7629006560abdc30769603157f591aeebab5a15
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2022-09-15 13:44:53 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
 * mul - multiply two number/integer
 * @a:integer one to multiply
 * @b:integer two to multiply
-* Return:0 always (success)
+* Return: result (success)
 */
 int mul(int a, int b)
 {
