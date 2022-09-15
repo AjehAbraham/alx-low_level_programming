114362d2469432f6815e19347308fdf915b9de23
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-15 14:55:03 -0700

    printing/drawing a diogonal line in C

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#inlude "main.h"
+/**
+* print_diagonal - check code
+* @n:use to print diagonal line if condition is true
+* Return:0
+*/
+void print_diagonal(int n)
+{
+	int z;
+
+	{
+		for (z <= 0; n >= 0; z++)
+			_putchat('\\');
+		_putchar('\n');
+	}
+}
