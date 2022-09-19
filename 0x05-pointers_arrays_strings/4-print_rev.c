721a7ea1effff481d5e70c07c6003cff28a1d5ce
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-19 05:24:24 -0700

    printing strings in reverse format

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_rev - check code to print string in reverse
+* @s:parameter use to check code
+* Return:0 alwyas(success)
+*/
+void print_rev(char *s)
+{
+	int R = 0;
+
+	while (R[s] >= 0)
+	{
+		if (R[s] == '\0')
+			break;
+		R++;
+	}
+	for (R--; R >= 0; i--)
+
+	_putchar(R[s]);
+	_putchar('\n');
+}
