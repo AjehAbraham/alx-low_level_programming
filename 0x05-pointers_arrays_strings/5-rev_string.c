b1f06f9673a67667d04fa19f3842dcb6f078c89f
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2022-09-19 14:17:18 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * rev_string - check code to reverse a string
-* @s:parameter use to check code 
+* @s:parameter use to check code
 * Return:0 always(success)
 */
 void rev_string(char *s)
