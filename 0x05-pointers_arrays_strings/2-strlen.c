10f98ae19b4f8b585d566abd117a4b866bd70aa1
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-19 04:00:41 -0700

    printing the length of a string using a ponter

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - check code to retrun length of a function
+* @s:use as parameter in function to print.check code
+* Return:0(success) always
+*/
+int _strlen(char *s)
+{
+	int b = 0;
+
+	while (*s[c] != 0)
+	{
+		b++
+	}
+			return (b);
+}
