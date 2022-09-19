439ece71b42bd941537287e4bdb3a68504a2ff6c
Ajeh Abraham <Ajehabraham51@gmail.com>
2022-09-19 03:30:31 -0700

    a function that point to the parameter n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - check code to print pointer
+* @n:use value to print/check for pointer
+* Return:0 always(success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
