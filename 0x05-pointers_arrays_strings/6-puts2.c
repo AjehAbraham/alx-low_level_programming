7a0168e61e5a8786dcdbd37be0ab46c3816abee0
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2022-09-19 14:18:28 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,19 @@
 */
 void puts2(char *str)
 {
-	int b = 0;
 
-	while (b >= 0)
+int count = 0;
+
+	while (count >= 0)
 	{
-		if (str[b] == '\0')
+		if (str[count] == '\0')
 		{
 			_putchar('\n');
-				break;
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
-	if (b % 2 == 0)
-		_putchar(str[b]);
-	b++
-	}
+
 }
