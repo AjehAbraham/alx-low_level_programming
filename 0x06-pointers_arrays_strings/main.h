b8ca49172e977d49d01366e89a5c78a8f573cd8a
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2022-09-22 15:39:13 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
-#ifndef "main.h"
-#define "main.h"
+#ifndef main_h
+#define main_h
+extern char *_strcat(char *dest, char *src);
+extern char *_strncat(char *dest, char *src, int n);
+extern char *_strncpy(char *dest, char *src, int n);
+extern int _strcmp(char *s1, char *s2);
+extern void reverse_array(int *a, int n);
+extern char *string_toupper(char *);
+extern char *cap_string(char *);
 #endif
